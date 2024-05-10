fe6c07ecad7c0f63491e809145fc0a1b7751c603
andhikay24 <andhika.perdana@btpn.com>
2024-05-10 23:21:40 +0700

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'class-methods-use-this': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'no-unsafe-optional-chaining': 'off',
+    'eslint-disable eqeqeq': 'off',
+    'import/no-extraneous-dependencies': 'off'
+  },
+};
