34b4a9f718002b53643b0f58a4de3cb2323b4a57
andhikay24 <andhika.perdana@btpn.com>
2024-05-10 23:23:49 +0700

    add form group component

---
+++ src/component/FormGroup/FormGroup.component.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+const FormGroup = (props) => {
+  const {
+    controlId, label, name, register, errors, type, prefix, placeholder
+  } = props;
+  const { onChange, onBlur, ref } = register(name);
+
+  const renderFormControl = () => (
+    <Form.Control
+      type={type}
+      onChange={onChange}
+      ref={ref}
+      onBlur={onBlur}
+      name={name}
+      placeholder={placeholder}
+    />
+  );
+
+  const renderFormWithPrefix = () => (
+    <InputGroup>
+      <InputGroup.Text>
+        {prefix}
+      </InputGroup.Text>
+      {renderFormControl()}
+    </InputGroup>
+  );
+
+  return (
+    <Form.Group controlId={controlId} className="mb-3">
+      <Form.Label>{label}</Form.Label>
+      {prefix ? renderFormWithPrefix() : renderFormControl()}
+      {errors[name] && <Form.Text className="text-danger">{errors[name].message}</Form.Text>}
+    </Form.Group>
+  );
+};
+
+export default FormGroup;
